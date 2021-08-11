cef24da3e4137b01293b024715ac175fe4527a3f
vlada <vladimir1987.markovic@gmail.com>
2021-08-11 16:11:41 +0200

    Final commit -adding some tweks

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $all_result = $myRes = $reseult->getAllStudents();
 	<title>Calculate Students Grades</title>
 	<center><h1 style="font-size: 42px;">Studnets With Grades</h1><center>
 	<center><ol style="list-style-position: inside; font-size: 32px;">
-		<?php foreach ($all_result as $res):?//foreash hole database to get all students
+		<?php foreach ($all_result as $res):?>//foreash hole database to get all students
 		<li><a href="single_student_result.php/<?=$res->id;?>"><?=$res->name; ?></a></li>//linking sudent to single page
 	<?php endforeach ?>
 	</ol></center>
