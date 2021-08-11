70b8e22a3655b9b4f629827bf17091c7c2bfcb8d
vlada <vladimir1987.markovic@gmail.com>
2021-08-11 16:14:52 +0200

    Commit more tweks

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once 'Class/Single-Sudent.php'; // call to class to get all stundents as list
+include_once 'Class/Single-Student.php'; // call to class to get all stundents as list
 $reseult = new SingleStudent();
 $all_result = $myRes = $reseult->getAllStudents();
 
@@ -13,8 +13,8 @@ $all_result = $myRes = $reseult->getAllStudents();
 	<title>Calculate Students Grades</title>
 	<center><h1 style="font-size: 42px;">Studnets With Grades</h1><center>
 	<center><ol style="list-style-position: inside; font-size: 32px;">
-		<?php foreach ($all_result as $res):?>//foreash hole database to get all students
-		<li><a href="single_student_result.php/<?=$res->id;?>"><?=$res->name; ?></a></li>//linking sudent to single page
+		<?php foreach ($all_result as $res):?><!--foreash hole database to get all students-->
+		<li><a href="single_student_result.php/<?=$res->id;?>"><?=$res->name; ?></a></li><!--linking sudent to single page-->
 	<?php endforeach ?>
 	</ol></center>
 </head>
